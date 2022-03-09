c39401f8899fa1ff1906de5bcb2d2be78a46ed13
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-03-09 10:53:41 -0300

    Seed Contact, Company and User using 'faker' gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,53 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Use faker to seed
+require 'faker'
+
+# Seed from a CSV file
+# require 'csv'
+
+# Delete everything
+Contact.delete_all
+Company.delete_all
+User.delete_all
+
+# Create companies
+50.times do
+  company = Company.new(
+    name: Faker::Company.unique.name
+  )
+  company.save!
+end
+
+# Create users
+20.times do
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.unique.last_name
+  user = User.new(
+    first_name: first_name,
+    last_name: last_name,
+    job_title: Faker::Job.title,
+    email: "#{first_name.parameterize(separator: '_')}.#{last_name.parameterize(separator: '_')}@smuca.fr",
+    description: Faker::Lorem.paragraph,
+    phone: Faker::Base.with_locale('fr') { Faker::PhoneNumber.cell_phone_with_country_code },
+    password: 'password',
+    password_confirmation: 'password'
+  )
+  user.save!
+end
+
+# Create contacts
+200.times do
+  first_name = Faker::Name.unique.first_name
+  last_name = Faker::Name.unique.last_name
+  contact = Contact.new(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name}.#{last_name.parameterize(separator: '_')}@#{Faker::Internet.domain_name}",
+    phone: Faker::Base.with_locale('fr') { Faker::PhoneNumber.cell_phone_with_country_code },
+    company: Company.all.sample
+  )
+  contact.save!
+end
