593ef12c03d127f207da99df7738fe871681c308
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-03-09 16:05:39 -0300

    Properly set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,28 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Error pages
+
   match '/404', to: 'errors#not_found', via: :all
+
   match '/500', to: 'errors#internal_server_error', via: :all
 
   # Defines the root path route ("/")
+
   root 'pages#show', page: 'home'
 
-  # Pages routes
-  get '/:page', to: 'pages#show'
-
   # Devise routes
+
   devise_for :users
 
   # Users routes
+
   resources :users
 
   # Contacts routes
+
   resources :contacts
 
   # Companies routes
+
   resources :companies
 end
