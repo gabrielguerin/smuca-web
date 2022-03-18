7fdf4ef32141e94e1246c2d72a206ba38a949bd9
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-03-18 12:47:40 -0300

    Add daisyui config to tailwind's config file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,5 +18,16 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require("daisyui")
-  ]
+  ],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
