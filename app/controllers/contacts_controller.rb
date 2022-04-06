c9b776565fe439e00aaa92a5e0fd8d83f2938968
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-04-05 20:49:49 -0400

    Remove unnecessary methods from contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
-  before_action :set_contact, only: %i[show edit update destroy]
-
-  # GET /contacts or /contacts.json
-
-  def index
-    @contacts = Contact.all
-  end
-
-  # GET /contacts/1 or /contacts/1.json
-
-  def show; end
-
   # GET /contacts/new
 
   def new
@@ -20,10 +8,6 @@ class ContactsController < ApplicationController
 
     @contact.build_company
   end
-
-  # GET /contacts/1/edit
-
-  def edit; end
 
   # POST /contacts or /contacts.json
 
@@ -50,45 +34,7 @@ class ContactsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contacts/1 or /contacts/1.json
-
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-
-        format.html { redirect_to contact_url(@contact), notice: 'Contact was successfully updated.' }
-
-        format.json { render :show, status: :ok, location: @contact }
-
-      else
-
-        format.html { render :edit, status: :unprocessable_entity }
-
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-
-      end
-    end
-  end
-
-  # DELETE /contacts/1 or /contacts/1.json
-
-  def destroy
-    @contact.destroy
-
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
-
-      format.json { head :no_content }
-    end
-  end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-
-  def set_contact
-    @contact = Contact.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
 
