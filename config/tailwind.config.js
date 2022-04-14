55695ef7a567937f338fe6a91ab9b67d4ec03090
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-04-14 19:07:26 -0400

    Remove daisyui from tailwind config file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,15 +19,4 @@ module.exports = {
     require('@tailwindcss/typography'),
     require("daisyui")
   ],
-  // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
 }
