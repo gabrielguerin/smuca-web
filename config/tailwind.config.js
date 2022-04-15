00c0455432f1d06dd0653aa477e5142dc0f147b2
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-04-15 16:33:11 -0400

    Require Flowbite as a plugin inside the tailwind.config.js file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('tw-elements/dist/plugin')
+    require('tw-elements/dist/plugin'),
+    require('flowbite/plugin')
   ],
 }
