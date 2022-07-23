932e4987600f583b1421ce5acf885dfe5f9b1640
Gabriel Guérin <gabriel.gueringg@gmail.com>
2022-07-23 19:58:03 +0200

    Hide specific record from All select * type queries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,27 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
+
   def index
-    @users = User.all
+    # Hide specific record from All select * type queries
+
+    @users = User.all.where.not(email: 'super.admin@smuca.fr')
   end
 
   # GET /users/1 or /users/1.json
+
   def show; end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
+
   def set_user
     @user = User.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :job_title, :description, :phone)
   end
